a981f4028fd9b77da843919533ed37b0ed13002d
CircleCI <circleci@data.gouv.fr>
2018-10-11 18:53:27 +0000

    Deploy doc.data.gouv.fr

---
+++ snippets/api/convention.httpie.sh
@@ -0,0 +1,5 @@
+# All HTTPie snippets assume the following declarations
+# HTTPie needs to be installed
+export API_KEY = 'my-api-key'
+export ORG = '5bbb6d6cff66bd4dc17bfd5a'
+export API = 'https://www.data.gouv.fr/api/1'
