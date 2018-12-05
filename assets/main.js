5f5b779f20cefa613e1f384fee3c64c7eb73ad90
Axel Haustant <noirbizarre@gmail.com>
2018-12-05 21:37:54 +0100

    Add search templates, style and scripts (still hating Jekyll 😑)

--- assets/main.js
+++ assets/main.js
@@ -5,17 +5,40 @@
     [].forEach.call(container.querySelectorAll('h2,h3,h4,h5,h6'), function(title) {
       const use = document.createElementNS('http://www.w3.org/2000/svg', 'use');
       use.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', '#icon-link');
-      
+
       const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
       svg.appendChild(use);
-      
+
       const anchor = document.createElement('a');
       anchor.href = '#' + title.id;
       anchor.classList.add('anchor');
       anchor.appendChild(svg);
-      
+
       title.appendChild(anchor);
     });
   });
 
+  // Handle search
+  const searchInput = document.getElementById('search-input')
+
+  SimpleJekyllSearch({
+    searchInput: searchInput,
+    resultsContainer: document.getElementById('search-results-container'),
+    json: '/search.json',
+    searchResultTemplate: [
+      '<div class="search-result">',
+      '<h4><a href="{{ site.url }}{url}">{category} > {title}</a></h4>',
+      '<p>{excerpt}</p>',
+      '</div>'
+    ].join('')
+  })
+
+  searchInput.addEventListener('input', function(ev) {
+    if (searchInput.value) {
+      document.body.classList.add('searching')
+      document.getElementById('search-query').innerText = searchInput.value
+    } else {
+      document.body.classList.remove('searching')
+    }
+  })
 })();
