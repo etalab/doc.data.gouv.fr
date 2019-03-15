081d6befec2a4ee8cbf6a808acba9b61a7be6782
Alexandre Bulté <alexandre@bulte.net>
2019-03-15 10:10:02 +0100

    Fix curl snippet for resource file upload

--- snippets/api/upload-resource.curl.sh
+++ snippets/api/upload-resource.curl.sh
@@ -1,4 +1,4 @@
 curl -H "Accept:application/json" \
      -H "X-Api-Key:$API_KEY" \
-     -F "file=/chemin/vers/le/fichier" \
+     -F "file=@/chemin/vers/le/fichier" \
      -X POST $API/datasets/$DATASET/upload
